e0772c978baf1c814e4ba54c89c86ea14d853edb
Patamavadee Tubthong <104760464+patamavadee16@users.noreply.github.com>
2023-03-21 12:31:17 +0700

    first commit

---
+++ deployment/native/styles.js
@@ -0,0 +1,10 @@
+import * as style0 from "C:/Users/Admin/Mendix/TOEIC Vocabulary-main_3/themesource/administration/native/main";
+import * as style1 from "C:/Users/Admin/Mendix/TOEIC Vocabulary-main_3/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/Admin/Mendix/TOEIC Vocabulary-main_3/themesource/webactions/native/main";
+import * as style3 from "C:/Users/Admin/Mendix/TOEIC Vocabulary-main_3/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/Admin/Mendix/TOEIC Vocabulary-main_3/themesource/myfirstmodule/native/main";
+import * as style5 from "C:/Users/Admin/Mendix/TOEIC Vocabulary-main_3/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5]);
---
+++ deployment/react-web/start.ps1
@@ -0,0 +1,1 @@
+C:/Program Files/Mendix/9.22.0.62233\modeler\tools\node\node.exe C:/Program Files/Mendix/9.22.0.62233\modeler\tools\node\node_modules\rollup\dist\bin\rollup -c -w
